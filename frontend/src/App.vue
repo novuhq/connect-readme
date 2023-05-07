3d27507ad486fa8f7795ec50dfdfe6f59288715f
spaceofmiah <agbonzeosazuwa@gmail.com>
2023-05-07 19:43:59 +0100

    Add navbar and header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,47 +1,30 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <nav>
+    <h3>
+      <a id="brand">novuShop</a>
+    </h3>
 
+    <div>
+      <a>browse</a>
+      <a class="cta">Login</a>
+    </div>
+  </nav>
+
+
+  <header>
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <h1>Easy Shopping with Novu Recommend</h1>
+      <a>Browse Now</a>
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
