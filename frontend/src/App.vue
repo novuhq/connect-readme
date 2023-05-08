baa51f3b265983cdbfb29c6ac82cd4abfa95fe7d
spaceofmiah <agbonzeosazuwa@gmail.com>
2023-05-08 08:51:18 +0100

    Add cardlist component with some placeholder mock item

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import CardList from './components/CardList.vue';
 </script>
 
 <template>
@@ -13,7 +14,6 @@
     </div>
   </nav>
 
-
   <header>
     <div class="wrapper">
       <h1>Easy Shopping with Novu Recommend</h1>
@@ -22,9 +22,22 @@
   </header>
 
   <main>
-    
+    <div>
+      <h3 class="section-title" style='text-align:center'>Hot Sales</h3>
+      <CardList />
+    </div>
+
+    <div>
+      <h3 class="section-title" style='text-align:center'>Mouth Watering Campaign</h3>
+      <section>
+        
+      </section>
+    </div>
   </main>
 </template>
 
 <style scoped>
+ header div {
+  padding: 0 20px;
+ }
 </style>
---
+++ frontend/src/components/CardList.vue
@@ -0,0 +1,69 @@
+<template>
+	<section class="card-list">
+		<a class="item-card">
+			<span></span>
+			<h3>Hollo Dough</h3>
+			<small>$98</small>
+			<h6>buy</h6>
+		</a>
+
+		<a class="item-card">
+			<span></span>
+			<h3>Luda Base</h3>
+			<small>$98</small>
+			<h6>buy</h6>
+		</a>
+
+		<a class="item-card">
+			<span></span>
+			<h3>Hollo Dough</h3>
+			<small>$98</small>
+			<h6>buy</h6>
+		</a>
+
+		<a class="item-card">
+			<span></span>
+			<h3>Luda Base</h3>
+			<small>$98</small>
+			<h6>buy</h6>
+		</a>
+
+		<a class="item-card">
+			<span></span>
+			<h3>Hollo Dough</h3>
+			<small>$98</small>
+			<h6>buy</h6>
+		</a>
+
+		<a class="item-card">
+			<span></span>
+			<h3>Luda Base</h3>
+			<small>$98</small>
+			<h6>buy</h6>
+		</a>
+	</section>
+</template>
+
+
+<style scoped>
+.card-list {
+  padding: 4px 18px;
+  display: flex;
+	justify-content: space-evenly;
+	flex-wrap: wrap;
+  gap: 39px;
+}
+
+.item-card {
+	max-width: 300px;
+}
+
+.item-card span {
+	width: 180px;
+	height: 210px;
+	display: block;
+	max-width: 250px;
+	margin-bottom: 10px;
+	background-color: gray;
+}
+</style>
