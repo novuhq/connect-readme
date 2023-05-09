31cfa29d2c99c5ece7f8641e62262f03715c3f17
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2023-05-09 03:49:12 +0100

    update to the app file

--- backend-test/app.js
+++ backend-test/app.js
@@ -3,7 +3,7 @@ const { sequelize } = require("./sequel/models");
 const { User } = require("./sequel/models");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
