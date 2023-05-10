f834aaadb3d0543df0f216c82cbfcce7dd913f7c
spaceofmiah <agbonzeosazuwa@gmail.com>
2023-05-10 18:20:45 +0100

    Add explore view to router

--- frontend/src/routers/index.js
+++ frontend/src/routers/index.js
@@ -1,11 +1,13 @@
 import {createRouter, createWebHashHistory } from 'vue-router';
 
 import HomeView from '../components/views/HomeView.vue';
+import ExploreView from '../components/views/ExploreView.vue';
 import ProductDetailView from '../components/views/ProductDetailView.vue';
 
 
 const routes = [
     {path: '/', component: HomeView, name: 'home'}, 
+    {path: '/p', component: ExploreView, name: 'explore'},
     {path: '/p/:id(\\d+)/', component: ProductDetailView, name: 'productdetail'},
     { path: '/:pathMatch(.*)*', name: 'notfound', component: () => import('../components/NotFound.vue') },
 ]
