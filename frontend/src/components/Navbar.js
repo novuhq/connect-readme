bb3fe37768fd9bdd9dd02ccafd4261ae19014ddc
Ibimina hart <ibimina.c.hart@gmail.com>
2023-05-10 13:41:53 +0100

    docs: navbar component

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import { useState } from "react";
+import styles from "./navbar.module.css";
+function Navbar() {
+  const [isVisible, setIsVisible] = useState(false);
+  const toggleNav = () => {
+    setIsVisible(!isVisible);
+  };
+ 
+  return (
+    <header className={styles.header}>
+      <div className={styles.logoContainer}>
+        <Image src="/logo.png" width={30} height={30} alt="notification" />
+        <h1 className={styles.white}>
+          Stock<span className={styles.green}>alert</span>
+        </h1>
+      </div>
+
+      <nav className={styles.nav} data-visible={isVisible}>
+        <ul className={styles.navLinks}>
+          <li>Market</li>
+          <li>
+            {" "}
+            <button className={`${styles.btn} ${styles.login}`}>Login</button>
+          </li>
+          <li>
+            {" "}
+            <button className={`${styles.btn} ${styles.signIn}`}>
+              Sign in
+            </button>
+          </li>
+        </ul>
+      </nav>
+      <button
+        onClick={toggleNav}
+        className={styles.mobileNavigationBtn}
+        aria-label="menu"
+        aria-expanded={isVisible}
+      ></button>
+    </header>
+  );
+}
+
+export default Navbar;
