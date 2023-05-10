94bbebec0d1e085a961deea543031e07d71e35aa
spaceofmiah <agbonzeosazuwa@gmail.com>
2023-05-10 09:09:14 +0100

    Utilize router-link on navbar brand name

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav>
+    <h3>
+      <router-link to="/" id="brand">novuShop</router-link>
+    </h3>
+
+    <div>
+      <a>browse</a>
+      <a class="cta">Login</a>
+    </div>
+  </nav>
+</template>
