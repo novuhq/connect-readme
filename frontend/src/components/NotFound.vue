ab2eed3b2c2b40e5d3e74bf228781dd861a93e50
spaceofmiah <agbonzeosazuwa@gmail.com>
2023-05-10 09:09:28 +0100

    Add a not found component page

---
+++ frontend/src/components/NotFound.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2> Oops !!!  <br /> No such page found. Use recommended safe route </h2>
+        <router-link to="/">--> safe route</router-link>
+    </div>
+</template>
+
+<style scoped>
+div {
+    text-align: center;
+    line-height: 48px;
+    height: 90vh;
+    display:flex;
+    place-items:center;
+    flex-direction: column;
+    justify-content: center;
+}
+
+a {
+    text-decoration: none;
+    color: #ff006a;
+    font-weight: bolder;
+}
+</style>
