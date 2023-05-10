3c681975a046eac9256cdbbbf555c87fb1e54469
spaceofmiah <agbonzeosazuwa@gmail.com>
2023-05-10 10:11:50 +0100

    Add skeletal boiler plates for data fetching with composition API - YAGNI

--- frontend/src/components/views/ProductDetailView.vue
+++ frontend/src/components/views/ProductDetailView.vue
@@ -1,11 +1,20 @@
 <script setup> 
-    const props = defineProps({
-        name: String
-    })
+	import {watch} from 'vue';
+	import {useRouter, useRoute} from 'vue-router';
+
+	const route = useRoute()
+	
+	watch(
+		() => route.params.id, (newId) => {
+			// fetch data from API
+			console.log("New ID passed: "+ newId)
+		}
+	)
+
 </script>
 
 <template>
-    <div>
-        <h3>{{ $route.params }}</h3>
-    </div>
+	<div>
+		<h3>{{ $route.params }}</h3>
+	</div>
 </template>
