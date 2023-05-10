8a438865289c78631dff251ddce91df9c1237def
spaceofmiah <agbonzeosazuwa@gmail.com>
2023-05-10 09:13:59 +0100

    Include dummy product detail page

--- frontend/src/components/views/ProductDetailView.vue
+++ frontend/src/components/views/ProductDetailView.vue
@@ -1,3 +1,11 @@
-<template>
+<script setup> 
+    const props = defineProps({
+        name: String
+    })
+</script>
 
+<template>
+    <div>
+        <h3>{{ $route.params }}</h3>
+    </div>
 </template>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,16 @@
-import { createApp } from 'vue'
-import {createRouter, createWebHashHistory} from 'vue-router';
+import { createApp } from 'vue';
+import {createRouter, createWebHashHistory } from 'vue-router';
 
 import './assets/main.css';
-import App from './App.vue'
-import HomeView from './components/views/HomeView.vue'
+import App from './App.vue';
+import HomeView from './components/views/HomeView.vue';
+import ProductDetailView from './components/views/ProductDetailView.vue';
 
 
 const routes = [
-    {path: '/', component: HomeView}
+    {path: '/', component: HomeView, name: 'home'}, 
+    {path: '/p/:id(\\d+)/', component: ProductDetailView, name: 'productdetail'},
+    { path: '/:pathMatch(.*)*', name: 'notfound', component: () => import('./components/NotFound.vue') },
 ]
 
 const router = createRouter({
@@ -16,7 +19,5 @@ const router = createRouter({
 })
 
 const app = createApp(App)
-
 app.use(router)
-
 app.mount('#app')
