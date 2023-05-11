f3a380bea14dacd4e99811b1e11ae89b9f3df88d
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2023-05-11 13:27:48 +0100

    Add market list UI

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,24 +1,35 @@
 import Image from "next/image";
 import { useState } from "react";
 import styles from "./navbar.module.css";
+import Link from "next/link";
+import { useRouter } from 'next/router';
+
 function Navbar() {
   const [isVisible, setIsVisible] = useState(false);
+  const router = useRouter()
+  console.log(router.pathname)
   const toggleNav = () => {
     setIsVisible(!isVisible);
   };
  
   return (
     <header className={styles.header}>
-      <div className={styles.logoContainer}>
-        <Image src="/logo.png" width={30} height={30} alt="notification" />
-        <h1 className={styles.white}>
-          Stock<span className={styles.green}>alert</span>
-        </h1>
-      </div>
+       <Link href="/">
+        <div className={styles.logoContainer}>
+          <Image src="/logo.png" width={30} height={30} alt="notification" />
+          <h1 className={styles.white}>
+            Stock<span className={styles.green}>alert</span>
+          </h1>
+        </div>
+       </Link>
 
       <nav className={styles.nav} data-visible={isVisible}>
         <ul className={styles.navLinks}>
-          <li className={styles.navLink}>Market</li>
+          <li className={styles.navLink}>
+            <Link href="market" className={router.pathname === "/market" ? styles.activeNav : null }>
+            Market
+            </Link>
+          </li>
           <li className={styles.navLink}>
             {" "}
             <button className={`${styles.btn} ${styles.login}`}>Login</button>
