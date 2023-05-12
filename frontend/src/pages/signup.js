30b345f22978b5fba7841dd7c256271e5e44fcbd
Ibimina hart <ibimina.c.hart@gmail.com>
2023-05-12 08:57:47 +0100

    docs: sign up page

---
+++ frontend/src/pages/signup.js
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import styles from "../styles/sign.module.css";
+import Navbar from "../components/Navbar";
+function SignUp() {
+  return (
+    <div className={styles.signUpContainer}>
+      <Navbar />
+      <section className={styles.formContainer}>
+        <form className={styles.signUpForm}>
+          <h2 className={`${styles.center} ${styles.heading}`}>
+            Sign up to create an account
+          </h2>
+          <p className={`${styles.center} ${styles.subtext}`}>
+            {" "}
+            Please login to your account.
+          </p>
+          <label className={styles.label}>
+            <span className={styles.labelText}> Email</span>
+            <input
+              type="email"
+              placeholder="Enter your Email"
+              className={styles.input}
+            />
+          </label>
+          <label className={styles.label}>
+            <span className={styles.labelText}>Password</span>
+            <input
+              type="password"
+              placeholder="Enter your Password"
+              className={styles.input}
+            />
+          </label>
+          <button type="submit" className={styles.signUpBtn}>
+            Sign up
+          </button>
+          <p className={`${styles.center} ${styles.signupRedirect}`}>
+            Already have an account?{" "}
+            <Link href="/login" className={styles.signup}>
+              Login
+            </Link>
+          </p>
+        </form>
+      </section>
+    </div>
+  );
+}
+
+export default SignUp;
