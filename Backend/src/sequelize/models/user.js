a940d8503882f45ea1b18846d2c0854f6144f956
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2023-05-13 07:39:00 +0100

    created the migrations

---
+++ Backend/src/sequelize/models/user.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      unique: true
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
